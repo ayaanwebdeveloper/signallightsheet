ae18a070315fdce30ca609d0e6c10dd9be769eae
AyaanWebDeveloper <developwithayaan@gmail.com>
2024-12-09 04:01:55 -0800

    enter a signal light name

---
+++ app.js
@@ -0,0 +1,21 @@
+var t = prompt("enter signal color")
+
+if(t == "red"){
+    document.write("Must Stop");
+    
+}
+else if (t == "yellow"){
+    document.write("Ready To Move");
+
+}
+else if(t == "green"){
+    document.write("Move");
+
+}
+else{
+    document.write("Not Valid Only Green,Red,Yellow");
+}
+
+
+
+
